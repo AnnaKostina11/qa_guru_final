c360209ac7713385911148c35c68af4c4df0d905
Anna Kostina <kostinaanna11.02@gmail.com>
2025-12-14 17:13:30 +0300

    git push

--- tests/tests_UI_saucedemo/conftest.py
+++ tests/tests_UI_saucedemo/conftest.py
@@ -1,13 +1,15 @@
 import os
 
 import pytest
-from selene import browser
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene import browser
 from dotenv import load_dotenv
 from automation_exercise.utils import attach
+
+import allure
+import config
 
 DEFAULT_BROWSER_VERSION = "128.0"
 BASE_URL = os.getenv('OFF_BASE_URL', 'https://world.openfoodfacts.org')
@@ -61,3 +63,78 @@ def setup_browser(request):
     attach.add_video(browser)
 
     browser.quit()
+
+
+@allure.title('Инициализация браузера.')
+@pytest.fixture(scope='function', autouse=True)
+def manage_browser(request):
+    # Настройка конфигурации Selene
+    browser.config.base_url = os.getenv('BASE_URL', 'https://www.saucedemo.com')
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.timeout = 10.0
+
+    is_remote = request.config.getoption('remote')
+
+    if is_remote:
+        selenoid_login = os.getenv('SELENOID_LOGIN')
+        selenoid_pass = os.getenv('SELENOID_PASS')
+        selenoid_url = os.getenv('SELENOID_URL', 'selenoid.default.svc.cluster.local')
+
+        selenoid_capabilities = {
+            'browserName': 'chrome',
+            'browserVersion': '128.0',
+            'selenoid:options': {
+                'enableVideo': True,
+                'enableVNC': True,
+                'screenResolution': '1920x1080x24'
+            }
+        }
+
+        options = Options()
+        options.set_capability('selenoid:options', selenoid_capabilities['selenoid:options'])
+
+        # Selene автоматически подхватит драйвер
+        browser.config.driver = webdriver.Remote(
+            command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
+            options=options
+        )
+    else:
+        # Локальный Chrome
+        options = Options()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        browser.config.driver = webdriver.Chrome(options=options)
+
+    yield  # Selene управляет браузером
+
+    # Финализация с Allure
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    browser.quit()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        item.status = "failed"
+    else:
+        item.status = "passed"
+
+
+@pytest.fixture(scope="class")
+def log_in_saucedemo(browser):
+    """Логин на saucedemo через Selene"""
+    from pages.authorization_page import AuthorizationPage
+    from pages.home_page import HomePage
+
+    auth = AuthorizationPage(browser)
+    auth.open_authorization_page()
+    auth.fill_username(config.SAUCEDEMO_LOGIN)
+    auth.fill_password(config.SAUCEDEMO_PASSWORD)
+    auth.submit()
+
+    HomePage(browser).verify_url()
