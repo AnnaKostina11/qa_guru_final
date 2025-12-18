d8883df802412113217d11948e3c3656258e1421
Anna Kostina <kostinaanna11.02@gmail.com>
2025-12-18 20:51:21 +0300

    Добавлен эксепшн по логам браузера

--- tests/tests_ui_saucedemo/conftest.py
+++ tests/tests_ui_saucedemo/conftest.py
@@ -40,12 +40,15 @@ def pytest_runtest_makereport(item, call):
         attachment_type=AttachmentType.HTML,
     )
 
-    logs = browser.driver.get_log("browser")
-    allure.attach(
-        "\n".join(str(x) for x in logs),
-        name="Browser console logs",
-        attachment_type=AttachmentType.TEXT,
-    )
+    try:
+        logs = browser.driver.get_log("browser")
+        allure.attach(
+            "\n".join(str(x) for x in logs),
+            name="Browser console logs",
+            attachment_type=AttachmentType.TEXT,
+        )
+    except AttributeError:
+        print("Browser logs unavailable")
 
     allure.attach(
         _video_url(browser.driver.session_id),
